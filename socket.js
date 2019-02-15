c237ff0201a932941b8e8d077cd423f757a7384f
rmc_productions <r.vleug@gmail.com>
2019-02-15 21:25:18 +0100

    no empty messages

--- socket.js
+++ socket.js
@@ -49,8 +49,8 @@ io.on("connection", socket => {
     // MESSAGE
     socket.on("message", (message, server, channel) => {
         if(users.has(socket.client.id)){
-            if(message.content.length < 2000){
-                if(message.content == " " || message.content.length < 1){
+            if(message < 2000){
+                if(message == " " || message.length < 1){
                     let msg = new chat.MESSAGE(message, users.get(socket.client.id), server, channel);
                     if(msg.content.startsWith("/")){
                         let invoke = msg.content.substr(1).split(" ")[0];
